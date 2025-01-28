d413b69aa63c8e3e018bb950220d465ca99a7e91
DOBY24 <doby960@gmail.com>
2025-01-28 22:01:21 +0100

    update

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -15,13 +15,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
 <script setup lang="ts">
 </script>
